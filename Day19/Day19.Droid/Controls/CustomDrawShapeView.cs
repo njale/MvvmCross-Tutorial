3d965907667d2eacafe879df7a3745862cd39798
Njål Eide <njal.eide@evry.com>
2017-01-17 14:32:33 +0100

    Added Day19.Droid

---
+++ Day19/Day19.Droid/Controls/CustomDrawShapeView.cs
@@ -0,0 +1,74 @@
+using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Day19.Core.ViewModels;
+using Color = Android.Graphics.Color;
+
+namespace Day19.Droid.Controls
+{
+    public class CustomDrawShapeView : View
+    {
+        private Shape _theShape;
+
+        public CustomDrawShapeView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public CustomDrawShapeView(Context context) : base(context)
+        {
+        }
+
+        public CustomDrawShapeView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public CustomDrawShapeView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        public CustomDrawShapeView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
+        {
+        }
+
+        public Shape TheShape
+        {
+            get { return _theShape; }
+            set
+            {
+                _theShape = value;
+                Invalidate();
+            }
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            var rect = new RectF(0,0,300,300);
+            switch (TheShape)
+            {
+                case Shape.Circle:
+                    canvas.DrawOval(rect, new Paint {Color = Color.Aqua});
+                    break;
+                case Shape.Square:
+                    canvas.DrawRect(rect, new Paint { Color = Color.Red });
+                    break;
+                case Shape.Triangle:
+                    var path = new Path();
+                    path.MoveTo(rect.CenterX(), 0);
+                    path.LineTo(0, rect.Height());
+                    path.LineTo(rect.Width(), rect.Height());
+                    path.Close();
+
+                    var paint =  new Paint() {Color = Color.Magenta};
+                    paint.SetStyle(Paint.Style.Fill);
+                    canvas.DrawPath(path, paint);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Day19/Day19.IOS/Controls/ShapeLabel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using Day19.Core.ViewModels;
+using Foundation;
+using UIKit;
+
+namespace Day19.IOS.Controls
+{
+    [Register("ShapeLabel")]
+    public class ShapeLabel : UILabel
+    {
+        private Shape _theShape;
+
+        public ShapeLabel()
+        {
+        }
+
+        public ShapeLabel(IntPtr handle) : base(handle)
+        {
+            
+        }
+
+
+        public ShapeLabel(RectangleF frame) : base(frame)
+        {
+
+        }
+
+        public Shape TheShape
+        {
+            get { return _theShape; }
+            set
+            {
+                _theShape = value;
+                UpdateUI();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            Text = TheShape.ToString();
+
+            switch (TheShape)
+            {
+                case Shape.Circle:
+                    TextColor = UIColor.Cyan;
+                    break;
+                case Shape.Square:
+                    TextColor = UIColor.Red;
+                    break;
+                case Shape.Triangle:
+                    TextColor = UIColor.Magenta;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
